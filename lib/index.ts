1c33167b6e983bb44a050cb356b7f82ea72b46e8
Heknon <development@heknon.com>
2022-07-17 21:42:59 +0300

    added hebrew disclaimer

--- lib/index.ts
+++ lib/index.ts
@@ -90,11 +90,28 @@ function registerEventHandlers(eventListener: BaileysEventEmitter, bot: BotClien
                 \nI DO NOT plan to but the possibility is there.\
                 \nIf you are not keen with this, do not send the bot messages.\
                 \nEnjoy my bot! Get started using: >>help\n\nP.S You can DM the bot.";
+                const joinMessageHebrew =
+                    "**התראה**\nהבוט מנוהל על ידי אדם.\
+                    \nבכך ברשותי האפשרות לצפות בהודעות בצ'אטים.\
+                    \n*אני לא* מתכנן לעשות זאת אך האפשרות קיימת.\
+                    \nאם אינך מעוניין בכך, אל תשלח לבוט הודעות.\
+                    \nתהנו מהבוט שלי!\
+                    \nכתבו >>עזרה כדי להתחיל להשתמש בו!";
                 await chatRepository.update(chatJid, {
                     $set: {sent_disclaimer: true},
                 });
                 chat = await chatRepository.get(chatJid, true);
-                await messagingService.reply(msg, joinMessage, false);
+                await messagingService.replyAdvanced(
+                    msg,
+                    {text: joinMessage, buttons: [{buttonText: {displayText: ">>help"}}]},
+                    false,
+                );
+
+                await messagingService.replyAdvanced(
+                    msg,
+                    {text: joinMessageHebrew, buttons: [{buttonText: {displayText: ">>עזרה"}}]},
+                    false,
+                );
             }
 
             // const [isExecutableCommand, commands] = await chat.isExecutableCommand(msg);
