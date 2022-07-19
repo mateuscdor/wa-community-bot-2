0491ec60ec31b68f7f67061403adc1e2637fdd84
Heknon <development@heknon.com>
2022-07-20 02:20:19 +0300

    number match fix

--- lib/command/commands/economy/utils.ts
+++ lib/command/commands/economy/utils.ts
@@ -40,7 +40,7 @@ export function buildBalanceChangeMessage(
  * @returns a list of numbers that exist in the string
  */
 export function extractNumbers(str: string): number[] {
-    console.log(`general match: ${str.match(/((-\d+)|\d+)[a-zA-Z]+/g)}`);
+    console.log(`general match: ${str.match(/((-\d+)|\d+)[a-zA-Z]*/g)}`);
     const numbers = (str.match(/((-\d+)|\d+)[a-zA-Z]+/g) ?? []).map((numData) => {
         const numStr = numData.match(/(-\d+)|\d+/g)?.[0] ?? "";
         console.log(`numStr: ${numStr}`);
