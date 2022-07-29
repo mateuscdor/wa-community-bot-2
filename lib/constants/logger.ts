1b68406ac9b1ddec2c3c774e0a16724a7311e6d5
Heknon <development@heknon.com>
2022-07-29 16:09:23 +0300

    added directory creations

--- lib/constants/logger.ts
+++ lib/constants/logger.ts
@@ -1,25 +1,37 @@
 import moment from "moment";
 import {createLogger, format, transports} from "winston";
 import pino from "pino";
+import fs from "fs";
+
+const logDirectory = "./logs";
+const botLogsDirectory = `${logDirectory}/bot`;
+const botStoreDirectory = `${logDirectory}/store`;
+const botTrafficDirectory = `${logDirectory}/traffic`;
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+fs.existsSync(botStoreDirectory) || fs.mkdirSync(botStoreDirectory);
+fs.existsSync(botTrafficDirectory) || fs.mkdirSync(botTrafficDirectory);
+fs.existsSync(botLogsDirectory) || fs.mkdirSync(botLogsDirectory);
 
 export const logger = createLogger({
     level: "info",
     exitOnError: false,
     format: format.json(),
-    transports: [new transports.File({filename: `../../logs/bot/${moment().format("DD-MM-YYYY-HH-mm-ss")}.log`})],
+    transports: [new transports.File({filename: `${botLogsDirectory}/${moment().format("DD-MM-YYYY-HH-mm-ss")}.log`})],
 });
 
 export const botTrafficLogger = pino(
     {
         level: "info",
     },
-    pino.destination(`./logs/traffic/${moment().format("DD-MM-YYYY-HH-mm-ss")}.log`),
+    pino.destination(`${botTrafficDirectory}/${moment().format("DD-MM-YYYY-HH-mm-ss")}.log`),
 );
 
 export const storeLogger = pino(
     {
         level: "info",
-        stream: "store"
+        stream: "store",
     },
-    pino.destination(`./logs/store/${moment().format("DD-MM-YYYY-HH-mm-ss")}.log`),
+    pino.destination(`${botStoreDirectory}/${moment().format("DD-MM-YYYY-HH-mm-ss")}.log`),
 );
